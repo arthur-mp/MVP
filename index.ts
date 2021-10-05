e531b0fbb954ca2eb1e14d5d63d388917db99829
Arthur Morais <arthurmoraispimentel7@gmail.com>
2021-10-05 15:39:16 -0300

    Deletando depedências e rotas não utilizadas

--- index.ts
+++ index.ts
@@ -4,23 +4,6 @@ const port = 3333
 
 app.use(express.json());
 
-//mapendo as rotas
-app.get('/user', (req, res) => {
-    res.send('GET');
-});
-
-app.post('/user', (req, res) => {
-    res.send("POST")
-})
-
-app.delete('/user', (req, res) => {
-    res.send("DELETE")
-})
-
-app.put('/user', (req, res) => {
-    res.send("PUT")
-})
-
 app.listen(port, () => {
     console.log("Rodando a aplicação em: http://localhost:" + port);
 });
